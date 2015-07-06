787e55dc86e6194f012d46f3bc341724a1fc2578
Leo <Leo@bouloc.eu>
2015-07-06 01:33:00 -0400

    seemed to have fixed it

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+import os
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     raise ImportError(
         "Please upgrade `setuptools` to the newest version via: "
@@ -25,9 +26,19 @@ if __name__ == "__main__":
         'install_requires': install_requires,
         'tests_require': [],
         'packages':find_packages(),
+
+        # trying to add files...
+        'include_package_data':True,
+        'package_data':{'': ['*.txt']},
+
         'scripts': ['bin/csv_detective'],
         'name': 'csv_detective',
         'long_description': readme,
     }
+    
+    # Write list of all packages in text file
+    with open(os.path.join('csv_detective', 'all_packages.txt'), 'wb') as f:
+        for x in find_packages():
+            f.write(x + '\n')
 
     setup(**config)
