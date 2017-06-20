9182a664adfb95ec482da589dafd106261fb815f
Christian Quest <cquest@openstreetmap.fr>
2017-06-20 17:41:32 +0100

    ajout impasse, promenade, montee, rocade

--- csv_detective/detect_fields/FR/geo/code_departement/__init__.py
+++ csv_detective/detect_fields/FR/geo/code_departement/__init__.py
@@ -9,11 +9,9 @@ PROPORTION = 1
 def _is(val):
     '''Renvoie True si val peut être un code_département, False sinon'''
     val = val.zfill(3)
-    liste_des_dep = [str(x).zfill(3) for x in range(1,96)] + \
-                    ['02A', '02B'] + [str(x).zfill(3) for x in range(971,976)]
+    liste_des_dep = [str(x).zfill(3) for x in range(1,19)] + \
+                    ['02A', '02B'] +  \
+                    [str(x).zfill(3) for x in range(21,95)] + \
+                    [str(x).zfill(3) for x in range(971,976)] # DOM
     # TODO: Enregistrer la liste des départements dans un fichier texte séparé
     return val in liste_des_dep
-
-
-
-
