a36509d3fb280cf20147f28228aa51cdd01898c2
Christian Quest <cquest@openstreetmap.fr>
2017-06-20 17:42:57 +0100

    ajout regexp sur code INSEE communes

--- csv_detective/detect_fields/FR/geo/code_commune_insee/__init__.py
+++ csv_detective/detect_fields/FR/geo/code_commune_insee/__init__.py
@@ -8,12 +8,18 @@ PROPORTION = 1
 
 def _is(val):
     '''Renvoie True si val peut être un code commune INSEE, False sinon'''
-        # TODO : ajouter une regex pour : 'que des chiffres ou bien commence par 2A, 2B puis 3 chiffres'
+    # test sur la longueur
     if not len(val) in [4,5]:
         return False
+    # ajout 0 au début si nécessaire (perte courante avec tableurs)
     val = val.zfill(5)
+
+    # vérification de cohérence avec prise en compte corse 2A/2B et DOM (971-976 sauf 975)
+    regex = r'^([01345678][0-9]{4}|2[AB1-9][0-9]{3}|9([0-5][0-9]{3}|7[12346][0-9]{2}))$'
+    if not bool(re.match(regex, val)):
+        return False
+
     f = open(join(dirname(__file__), 'code_commune_insee.txt'), 'r')
     liste = f.read().split('\n')
     f.close()
     return val in liste
-
