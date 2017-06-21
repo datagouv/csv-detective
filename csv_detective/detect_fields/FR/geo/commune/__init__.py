95f71698bde711d262ca1c7130f3ba22ba8f13d2
Christian Quest <cquest@openstreetmap.fr>
2017-06-21 17:28:11 +0100

    nom commune un peu plus souple

--- csv_detective/detect_fields/FR/geo/commune/__init__.py
+++ csv_detective/detect_fields/FR/geo/commune/__init__.py
@@ -2,10 +2,11 @@ from os.path import dirname, join
 from csv_detective.process_text import _process_text
 import re
 
-PROPORTION = 1
+PROPORTION = 0.9
 
 def _is(val):
     '''Match avec le nom des communes'''
+    val = val.lower().replace('-',' ')
     f = open(join(dirname(__file__), 'commune.txt'), 'r')
     liste = f.read().split('\n')
     f.close()
