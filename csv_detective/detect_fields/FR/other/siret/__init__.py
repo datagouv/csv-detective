c20a0413950727fba2faf54a311854890af0751e
Christian Quest <cquest@openstreetmap.fr>
2017-06-21 11:03:26 +0100

    détection identifiants SIRET (WIP)

---
+++ csv_detective/detect_fields/FR/other/siret/__init__.py
@@ -0,0 +1,32 @@
+from os.path import dirname, join
+from csv_detective.process_text import _process_text
+import re
+
+PROPORTION = 1
+
+
+def _is(val):
+    '''Détection des identifiants SIRET (SIRENE)'''
+    val = val.replace(' ', '')
+    regex = r'^[0-9]{14}$'
+    if not bool(re.match(regex, val)):
+        return False
+
+    # Vérification par clé de luhn du SIREN
+    cle = 0
+    pair = False
+    for x in val[:9]:
+        y = int(x) * (1 + pair)
+        cle += y // 10 + y % 10
+        pair = not pair
+    if cle % 10 != 0:
+        return cle % 10 == 0
+
+    # Vérification par clé de luhn du SIRET
+    cle = 0
+    pair = False
+    for x in val:
+        y = int(x) * (1 + pair)
+        cle += y // 10 + y % 10
+        pair = not pair
+    return cle % 10 == 0
