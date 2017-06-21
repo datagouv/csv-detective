94f73579813a0aac0b4d891ec5ec10965079892b
Christian Quest <cquest@openstreetmap.fr>
2017-06-21 18:11:07 +0100

    nouveaux détecteurs mois, lat, lon

--- csv_detective/detect_fields/__init__.py
+++ csv_detective/detect_fields/__init__.py
@@ -2,7 +2,7 @@ from .FR.other import code_csp_insee, csp_insee, sexe, siren, tel_fr, uai, siret
 from .other import email, url
 
 from .FR.geo import adresse, code_commune_insee, code_postal, commune, departement, pays, region, code_departement
-from .geo import iso_country_code
+from .geo import iso_country_code,latitude_wgs,longitude_wgs
 
-from .FR.temp import jour_de_la_semaine
+from .FR.temp import jour_de_la_semaine,mois_de_annee
 from .temp import year, date
