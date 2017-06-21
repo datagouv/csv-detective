449d7de8ea0f10016a9129112780daaaf24b0316
Christian Quest <cquest@openstreetmap.fr>
2017-06-21 16:38:31 +0100

    correction détection CP

--- csv_detective/detect_fields/FR/geo/code_postal/__init__.py
+++ csv_detective/detect_fields/FR/geo/code_postal/__init__.py
@@ -2,23 +2,16 @@ from os.path import dirname, join
 from csv_detective.process_text import _process_text
 import re
 
-PROPORTION = 0.9
+PROPORTION = 1
 
 def _is(val):
     '''Renvoie True si val peut être un code postal, False sinon'''
-    if isinstance(val, str) or isinstance(val, unicode): # Si val est un string, on essaye de le convertir en nombre
-        if val.isdigit():
-            val = int(val)
-        else:
-            return False
-    else:
+
+    regex = r'^[0-9]{5}$'
+    if not bool(re.match(regex, val)):
         return False
-    if not (val > 1000) and (val < 100000):
-        return False
+
     f = open(join(dirname(__file__), 'code_postal.txt'), 'r')
     liste = f.read().split('\n')
     f.close()
-    return str(val).zfill(5) in liste
-
-
-
+    return val.zfill(5) in liste
