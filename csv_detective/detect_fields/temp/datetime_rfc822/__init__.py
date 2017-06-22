535746a7b78f5e7a3e82ae14a57feddf3ecd0a8a
Christian Quest <cquest@openstreetmap.fr>
2017-06-22 10:55:54 +0100

    détection datetime rfc822

---
+++ csv_detective/detect_fields/temp/datetime_rfc822/__init__.py
@@ -0,0 +1,16 @@
+from csv_detective.process_text import _process_text
+import re
+from dateutil.parser import parse
+
+PROPORTION = 1
+
+def _is(val):
+    '''Renvoie True si val peut être une date au format rfc822, False sinon
+    AAAA-MM-JJ HH-MM-SS avec indication du fuseau horaire
+
+    '''
+
+    val = val.lower()
+    a = bool(re.match(r'^((mon|tue|wed|thu|fri|sat|sun),|)\d{1,2} (jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec) \d\d(\d\d|) \d\d\:\d\d(\:\d\d|) (ut|gmt|est|edt|cst|cdt|mst|mdt|pst|pdt|([+-]\d{4}))$', val))
+
+    return a
