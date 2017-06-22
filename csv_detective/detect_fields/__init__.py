a86383e0ec341bbdc7ea6845d268cd7002821cd2
Christian Quest <cquest@openstreetmap.fr>
2017-06-22 10:56:55 +0100

    nouveaux détecteurs json_geojson latlon_wgs datetime_iso datetime_rfc822

--- csv_detective/detect_fields/__init__.py
+++ csv_detective/detect_fields/__init__.py
@@ -2,7 +2,7 @@ from .FR.other import code_csp_insee, csp_insee, sexe, siren, tel_fr, uai, siret
 from .other import email, url
 
 from .FR.geo import adresse, code_commune_insee, code_postal, commune, departement, pays, region, code_departement, code_fantoir
-from .geo import iso_country_code,latitude_wgs,longitude_wgs
+from .geo import iso_country_code,latitude_wgs,longitude_wgs,latlon_wgs,json_geojson
 
 from .FR.temp import jour_de_la_semaine,mois_de_annee
-from .temp import year, date
+from .temp import year, date, datetime_iso, datetime_rfc822
