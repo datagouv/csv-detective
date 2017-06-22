cce7b0187526b944e820e02142a561f96f824833
Christian Quest <cquest@openstreetmap.fr>
2017-06-22 10:54:54 +0100

    détection lat,lon (opendatasoft)

---
+++ csv_detective/detect_fields/geo/latlon_wgs/__init__.py
@@ -0,0 +1,11 @@
+from csv_detective.process_text import _process_text
+import re
+
+PROPORTION = 0.9
+
+def _is(val):
+    '''Renvoie True si val peut etre une latitude,longitude'''
+
+    a = bool(re.match(r'^[\+\-]?[0-8]\d\.\d* ?, ?[\+\-]?(1[0-7]\d|\d{1,2})\.\d+$', val))
+
+    return a
