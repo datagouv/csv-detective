778e8273676d0e776272932aa3f923bf1fed2f4d
Christian Quest <cquest@openstreetmap.fr>
2017-06-22 10:54:29 +0100

    détection geojson

---
+++ csv_detective/detect_fields/geo/json_geojson/__init__.py
@@ -0,0 +1,21 @@
+from csv_detective.process_text import _process_text
+import json
+
+PROPORTION = 0.9
+
+def _is(val):
+    '''Renvoie True si val peut etre geojson'''
+
+    print(val)
+
+    try:
+        j = json.loads(val)
+        if 'type' in j and 'coordinates' in j:
+            return True
+        if 'geometry' in j:
+            if 'coordinates' in j['geometry']:
+                return True
+    except:
+        pass
+
+    return False
