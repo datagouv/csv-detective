4782996741115aeec481b3b4cf74e420bc5420f0
Christian Quest <cquest@openstreetmap.fr>
2017-06-23 13:58:27 +0100

    ajout oui/non

--- csv_detective/detect_fields/other/booleen/__init__.py
+++ csv_detective/detect_fields/other/booleen/__init__.py
@@ -6,5 +6,5 @@ PROPORTION = 1
 
 def _is(val):
     '''Détection les booléens'''
-    liste_bool = ['0','1','vrai','faux','true','false']
+    liste_bool = ['0','1','vrai','faux','true','false','oui','non']
     return val.lower() in liste_bool
