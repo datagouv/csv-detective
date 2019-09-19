e63a8f9e34397f16b2fd8acb900699607dc719fb
Vincent <vincent.alvarez@data.gouv.fr>
2019-09-19 17:42:31 +0200

    modification des tests sur date et code departements

--- csv_detective/detect_fields/FR/geo/code_departement/__init__.py
+++ csv_detective/detect_fields/FR/geo/code_departement/__init__.py
@@ -6,8 +6,22 @@ PROPORTION = 1
 
 def _is(val):
     '''Renvoie True si val peut être un code_département, False sinon'''
-    liste_des_dep = [str(x).zfill(2) for x in range(1,19)] + \
-                    ['2A', '2B', '971','972','973','974','976'] +  \
-                    [str(x) for x in range(21,95)]
+    liste_des_dep = [str(x).zfill(2) for x in range(1,20)] + \
+                    ['2A', '2B', '971','972','973','974','976', '2a', '2b'] +  \
+                    [str(x) for x in range(21,96)]
     # TODO: Enregistrer la liste des départements dans un fichier texte séparé
     return val in liste_des_dep
+
+#from os.path import dirname, join
+#from csv_detective.process_text import _process_text
+import re
+
+#PROPORTION = 0.9
+
+#def _is(val):
+ #   '''Match avec le code des departements'''
+  #  f = open(join(dirname(__file__), 'code_ departement.txt'), 'r')
+  #  liste = f.read().split('\n')
+   # f.close()
+    #val = _process_text(val)
+    # return val in liste
