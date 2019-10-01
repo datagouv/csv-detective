713aaef7554d536764b875cc626b1e72f8eb42e9
Vincent <vincent.alvarez@data.gouv.fr>
2019-10-01 15:21:35 +0200

    création de la liste avec le fichier csp_insee.txt hors de la fonction _is pour augmenter la rapidité de traitement

--- csv_detective/detect_fields/FR/other/csp_insee/__init__.py
+++ csv_detective/detect_fields/FR/other/csp_insee/__init__.py
@@ -3,11 +3,13 @@ from csv_detective.process_text import _process_text
 import re
 
 PROPORTION = 1
+f = open(join(dirname(__file__), 'csp_insee.txt'), 'r')
+liste = f.read().split('\n')
+f.close()
+
 
 def _is(val):
     '''Repère les csp telles que définies par l'INSEE'''
     val = _process_text(val)
-    f = open(join(dirname(__file__), 'csp_insee.txt'), 'r')
-    liste = f.read().split('\n')
-    f.close()
+
     return val in liste
