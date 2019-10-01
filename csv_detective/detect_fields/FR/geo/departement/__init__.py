fb2fb02df3a56df2e6fbc4ccc594e122580bd979
Vincent <vincent.alvarez@data.gouv.fr>
2019-10-01 15:17:01 +0200

    création de la liste avec le fichier departement.txt hors de la fonction _is pour augmenter la rapidité de traitement

--- csv_detective/detect_fields/FR/geo/departement/__init__.py
+++ csv_detective/detect_fields/FR/geo/departement/__init__.py
@@ -3,11 +3,13 @@ from csv_detective.process_text import _process_text
 import re
 
 PROPORTION = 0.9
+f = open(join(dirname(__file__), 'departement.txt'), 'r')
+liste = f.read().split('\n')
+f.close()
+
 
 def _is(val):
     '''Match avec le nom des departements'''
-    f = open(join(dirname(__file__), 'departement.txt'), 'r')
-    liste = f.read().split('\n')
-    f.close()
+
     val = _process_text(val)
     return val in liste
