bd861ef54830f34292289609053572272a8d4bb7
Vincent <vincent.alvarez@data.gouv.fr>
2019-10-11 13:39:59 +0200

    ajout des tests code RNA/Code Waldec

--- csv_detective/detect_fields/FR/other/insee_ape700/__init__.py
+++ csv_detective/detect_fields/FR/other/insee_ape700/__init__.py
@@ -3,11 +3,13 @@ from csv_detective.process_text import _process_text
 import re
 
 PROPORTION = 1
+f = open(join(dirname(__file__), 'insee_ape700.txt'), 'r')
+liste = f.read().split('\n')
+f.close()
+
 
 def _is(val):
     '''Repère les codes APE700 de l'INSEE'''
     val = _process_text(val).upper()
-    f = open(join(dirname(__file__), 'insee_ape700.txt'), 'r')
-    liste = f.read().split('\n')
-    f.close()
+
     return val in liste
