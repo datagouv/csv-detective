b593ce69331bfa3bf49f5be9b41bae0454c126de
Pavel <pavel.soriano@data.gouv.fr>
2019-12-15 12:28:47 +0100

    Fixed random seed to get same result each run. Added general csv as test

---
+++ tests/test_csv.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Import the csv_detective package
+import json
+from pprint import pprint
+
+from csv_detective.explore_csv import routine
+
+
+def test_old_detection():
+    file_path = './annuaire-de-leducation.csv'
+    expected_results = json.load(open("baseline_result.json"))
+    # Open your file and run csv_detective
+    inspection_results = routine(file_path)
+    pprint(inspection_results)
+    assert str(inspection_results) == str(expected_results)
