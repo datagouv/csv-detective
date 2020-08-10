288a79189044f56e71f4e4eff8835ac57b892aca
Rob192 <reynaud.robin@gmail.com>
2020-08-10 17:02:54 +0200

    modify with argparse

--- analyse_csv.py
+++ analyse_csv.py
@@ -17,11 +17,13 @@ import datetime
 import json
 import os
 import logging
+import glob
+import argparse
 
 import joblib
 from joblib import Parallel, delayed
-from argopt import argopt
 from tqdm import tqdm
+import numpy as np
 
 from csv_detective.explore_csv import routine
 
@@ -30,6 +32,14 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 TODAY = str(datetime.datetime.today()).split()[0]
+
+
+def get_files(data_path, ext="csv", sample=None):
+    files = glob.glob(data_path + "**/*.{}".format(ext), recursive=True)
+    if sample:
+        return list(np.random.choice(files, sample, replace=False))
+    return files
+
 
 
 def analyze_csv(file_path, num_rows=500, date_process=TODAY,
@@ -48,22 +58,34 @@ def analyze_csv(file_path, num_rows=500, date_process=TODAY,
 
 
 if __name__ == '__main__':
-    parser = argopt(__doc__).parse_args()
-    csv_folder_path = parser.i
-    dest_folder = parser.j
-    date_process = parser.k
-    rb_ml_analysis = parser.rb_ml_analysis
-    num_files = parser.num_files
-    num_rows = parser.num_rows
-    n_jobs = int(parser.num_cores)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_folder',
+                        help='The folder where the csv can be found',
+                        required=True)
+    parser.add_argument('--output_folder',
+                        help='The folder where the output will be made',
+                        required=True)
+    parser.add_argument('--date_process',
+                        default=TODAY)
+    parser.add_argument('--num_files',
+                        default=10)
+    parser.add_argument('--num_rows',
+                        default=500)
+    parser.add_argument('--num_cores',
+                        default=1)
 
-    analysis_name = os.path.basename(os.path.dirname(csv_folder_path))
+    args = parser.parse_args()
+
+    print(args)
+
+
+    analysis_name = os.path.basename(os.path.dirname(args.input_folder))
     list_files = []
-    if os.path.exists(csv_folder_path):
-        if os.path.isfile(csv_folder_path):
-            list_files = [csv_folder_path]
+    if os.path.exists(args.input_folder):
+        if os.path.isfile(args.input_folder):
+            list_files = [args.input_folder]
         else:
-            list_files = get_files(csv_folder_path, sample=None)
+            list_files = get_files(args.input_folder, sample=None)
     else:
         logger.info("No file/folder found to analyze. Exiting...")
         exit(1)
@@ -72,23 +94,23 @@ if __name__ == '__main__':
         logger.info("No file/folder found to analyze. Exiting...")
         exit(1)
 
-    if n_jobs and n_jobs > 1:
-        csv_info = Parallel(n_jobs=n_jobs)((file_path.split("/")[-1].split(".csv")[0],
+    if args.num_cores and args.num_cores > 1:
+        csv_info = Parallel(n_jobs=args.num_cores)((file_path.split("/")[-1].split(".csv")[0],
                                             delayed(analyze_csv)(file_path,
-                                                                 num_rows=num_rows,
-                                                                 date_process=date_process))
+                                                                 num_rows=args.num_rows,
+                                                                 date_process=args.date_process))
                                            for file_path in tqdm(list_files))
     else:
         csv_info = []
         for file_path in tqdm(list_files):
             dataset_id = file_path.split("/")[-1].split(".csv")[0]
             analysis_output = analyze_csv(file_path,
-                                          num_rows=num_rows,
-                                          date_process=date_process,)
+                                          num_rows=args.num_rows,
+                                          date_process=args.date_process,)
             csv_info.append((dataset_id, analysis_output))
 
     logger.info("Saving info to JSON")
     # check that we have a good result
     #_check_full_report(analysis_output, logger)
-    json.dump(csv_info, open(f"{dest_folder}/{date_process}.json", "w"),
+    json.dump(csv_info, open(f"{args.output_folder}/{args.date_process}.json", "w"),
               indent=4, ensure_ascii=False)
