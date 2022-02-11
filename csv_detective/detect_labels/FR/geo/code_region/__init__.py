2f9ffc72ff1be19965e2f8fd5f93e17f618b1124
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-02-11 15:13:17 +0100

    Add item for code_region label detection

--- csv_detective/detect_labels/FR/geo/code_region/__init__.py
+++ csv_detective/detect_labels/FR/geo/code_region/__init__.py
@@ -6,7 +6,7 @@ PROPORTION = 1
 def _is(header):
     '''Returns 1 if the (processed) header matches one of the expected words combination, else 0'''
 
-    words_combinations_list = ['code region', 'reg', 'code insee region'] #'reg' : possible confusion with region name?
+    words_combinations_list = ['code region', 'reg', 'code insee region', 'region'] #'reg' : possible confusion with region name?
     processed_header = _process_text(header)
 
     header_matches_words_combination = float(any([words_combination == processed_header for words_combination in words_combinations_list]))
