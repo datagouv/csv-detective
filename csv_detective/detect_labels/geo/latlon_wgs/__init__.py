7252359119c2d0900812bbeed75deb7880488863
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-09-28 11:12:39 +0200

    support optional square brackets for latlon

--- csv_detective/detect_labels/geo/latlon_wgs/__init__.py
+++ csv_detective/detect_labels/geo/latlon_wgs/__init__.py
@@ -6,7 +6,10 @@ PROPORTION = 0.5
 def _is(header):
     '''Returns 1 if the (processed) header matches one of the expected words combination, else 0'''
 
-    words_combinations_list = ['latlon wgs', 'latlon', 'geo point', 'geo point 2d', 'wgs84', 'geolocalisation', 'geo', 'coordonnees finales', 'coordonnees', 'coordonnees ban', 'xy', 'geometry x y', 'coordonnees insee', 'coordonnees geographiques', 'position', 'coordonnes gps', 'geopoint', 'geom x y', 'coord gps', 'latlong', 'position geographique', 'c geo', 'coordonnes geoloc', 'lat lon', 'code geo', 'geo localisation', 'coordonnes geo', 'geo cp', 'x y', 'geo coordinates', 'point geo', 'point geo insee', 'coordonnees geoloc']
+    words_combinations_list = ['latlon wgs', 'latlon', 'geo point', 'geo point 2d', 'wgs84', 'geolocalisation', 'geo', 'coordonnees finales', 'coordonnees',
+        'coordonnees ban', 'xy', 'geometry x y', 'coordonnees insee', 'coordonnees geographiques', 'position', 'coordonnes gps', 'geopoint', 'geom x y',
+        'coord gps', 'latlong', 'position geographique', 'c geo', 'coordonnes geoloc', 'lat lon', 'code geo', 'geo localisation', 'coordonnes geo', 'geo cp',
+        'x y', 'geo coordinates', 'point geo', 'point geo insee', 'coordonnees geoloc', 'coordonnees xy']
     processed_header = _process_text(header)
 
     header_matches_words_combination = float(any([words_combination == processed_header for words_combination in words_combinations_list]))
