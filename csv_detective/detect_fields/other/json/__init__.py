5da47f451703df2e039b26750d4c4a9ac8652be6
Pierlou <pierlou.ramade@data.gouv.fr>
2023-02-06 14:41:53 +0100

    fix: recognize lists as json

--- csv_detective/detect_fields/other/json/__init__.py
+++ csv_detective/detect_fields/other/json/__init__.py
@@ -7,7 +7,9 @@ def _is(val):
     '''Detects json'''
     try:
         loaded = json.loads(val)
-        if isinstance(loaded, dict) and not(any([geo in loaded for geo in ['coordinates', 'geometry']])):
+        if isinstance(loaded, list) or (isinstance(loaded, dict) and not(any([geo in loaded for geo in ['coordinates', 'geometry']]))):
             return True
+        else:
+            return False
     except:
         return False
