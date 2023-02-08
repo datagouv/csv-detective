a45789dea112d4cc262972db15584184564942a1
Alexandre Bulté <alexandre@bulte.net>
2023-02-08 12:22:56 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -15,32 +15,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Information Analysis",
         ],
-    description = 'Detect CSV column content',
-    long_description_content_type='text/markdown',
-    keywords = 'CSV data processing encoding guess parser tabular',
-    license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
-    url = 'https://github.com/etalab/csv_detective',
-    data_files = [
-        ('share/csv_detective', ['CHANGELOG.md', 'LICENSE.AGPL.txt', 'README.md']),
-        ],
-    entry_points={
-        'console_scripts': [
-            'csv_detective=csv_detective.cli:run',
-            ],
-        },
-    extras_require = {
-        'test': [
-            'nose',
-            ],
-        },
-    include_package_data = True,  # Will read MANIFEST.in
-    install_requires = [
-        'pandas >= 0.20',
-        'boto3 >= 1.21.21',
-        'chardet >= 3.0',
-        'unidecode >= 0.4',
-        'cchardet',
-    ],
     description="Detect CSV column content",
     long_description_content_type="text/markdown",
     keywords="CSV data processing encoding guess parser tabular",
