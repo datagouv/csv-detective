dc0789dce09601e077be3adb7a9249c64873259e
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-22 11:31:03 +0200

    feat: Use frformat for pays

--- csv_detective/detect_fields/FR/geo/pays/__init__.py
+++ csv_detective/detect_fields/FR/geo/pays/__init__.py
@@ -1,17 +1,7 @@
-from os.path import dirname, join
-from csv_detective.process_text import _process_text
+from frformat import Pays
 
 PROPORTION = 0.6
-f = open(join(dirname(__file__), 'pays.txt'), 'r')
-pays = f.read().split('\n')
-pays = set(pays)
-max_len = max({len(p) for p in pays})
-f.close()
-
 
 def _is(val):
     '''Match avec le nom des pays'''
-    if len(val) > max_len:
-        return False
-    val = _process_text(val)
-    return val in pays
+    return Pays.is_valid(val)
