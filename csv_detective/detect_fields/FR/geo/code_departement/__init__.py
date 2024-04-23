c5a83938eccd46c66f5514aa6feb4a7d45674509
sarrabah <sarrabenarbia1996@gmail.com>
2024-04-23 10:56:43 +0200

    refactor: Use strict egale à false en numero de departement

--- csv_detective/detect_fields/FR/geo/code_departement/__init__.py
+++ csv_detective/detect_fields/FR/geo/code_departement/__init__.py
@@ -4,6 +4,5 @@ PROPORTION = 1
 
 
 def _is(val):
-    """Renvoie True si val peut être un code_département, False sinon"""
-    alternative_corsica = {"2a", "2b"}
-    return NumeroDepartement.is_valid(val) or val in alternative_corsica
+
+    return NumeroDepartement.is_valid(val, strict = False) 
