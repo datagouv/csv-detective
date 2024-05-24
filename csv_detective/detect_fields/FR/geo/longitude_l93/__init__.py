d847204d078061fc55613f4df22ccba05bac987d
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-24 19:34:29 +0200

    refactor: Integrate LongitudeL93 from frformat

--- csv_detective/detect_fields/FR/geo/longitude_l93/__init__.py
+++ csv_detective/detect_fields/FR/geo/longitude_l93/__init__.py
@@ -1,13 +1,24 @@
+from frformat import LongitudeL93
 from csv_detective.detect_fields.other.float import _is as is_float
+from csv_detective.detect_fields.other.float import float_casting
+
 
 PROPORTION = 0.9
 
 
 def _is(val):
-    '''Renvoie True si val peut etre une longitude en métropole'''
     try:
-        is_float(val) and float(val) >= -357823 and float(val) <= 1313633
+        if type(val) is float or type(val) is int:
+            return LongitudeL93.is_valid(val)
+
+        elif type(val) is str:
+            if is_float(val):
+                return LongitudeL93.is_valid(float_casting(val))
+
+        return False
+
     except ValueError:
         return False
     except OverflowError:
         return False
+
