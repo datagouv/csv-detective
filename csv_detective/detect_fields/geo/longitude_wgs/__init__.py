f79cafbfdc8f6815a74a94ec36e186bbb7ce8e54
Pierlou <pierlou.ramade@data.gouv.fr>
2024-10-11 09:21:56 +0200

    fix: return test instead of just trying

--- csv_detective/detect_fields/geo/longitude_wgs/__init__.py
+++ csv_detective/detect_fields/geo/longitude_wgs/__init__.py
@@ -6,7 +6,7 @@ PROPORTION = 0.9
 def _is(val):
     '''Renvoie True si val peut etre une longitude'''
     try:
-        is_float(val) and float(val) >= -180 and float(val) <= 180
+        return is_float(val) and float(val) >= -180 and float(val) <= 180
     except ValueError:
         return False
     except OverflowError:
