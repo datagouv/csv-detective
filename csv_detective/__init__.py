e1b609a57ea2c32140ac74d854ae4455ffdccc35
Pierlou <pierlou.ramade@data.gouv.fr>
2025-04-15 16:01:39 +0200

    refactor: move example into output folder

--- csv_detective/__init__.py
+++ csv_detective/__init__.py
@@ -1,4 +1,4 @@
 from .explore_csv import routine, routine_minio  # noqa
-from .create_example import create_example_csv_file  # noqa
+from .output.example import create_example_csv_file  # noqa
 
 __version__ = '0.7.5.dev'
