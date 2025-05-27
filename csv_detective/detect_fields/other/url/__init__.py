21a9219eb18af84a7536bc44bb264f9a4ec9b229
Pierlou <pierlou.ramade@data.gouv.fr>
2025-05-27 10:47:54 +0200

    reactor: better URL test

--- csv_detective/detect_fields/other/url/__init__.py
+++ csv_detective/detect_fields/other/url/__init__.py
@@ -1,13 +1,14 @@
+import re
 
 PROPORTION = 1
+url_pattern = re.compile(
+    r"^((https?|ftp)://|www\.)(([A-Za-z0-9-]+\.)+[A-Za-z]{2,6})"
+    r"(/[A-Za-z0-9._~:/?#[@!$&'()*+,;=%-]*)?$"
+)
 
 
 def _is(val):
-    '''Detects urls'''
+    """Detects urls"""
     if not isinstance(val, str):
         return False
-    a = 'http://' in val
-    b = 'www.' in val
-    c = any([x in val for x in ['.fr', '.com', '.org', '.gouv', '.net']])
-    d = not ('@' in val)
-    return (a or b or c) and d
+    return bool(url_pattern.match(val))
