07566bf47733048a2bab96d821ff003d78ec70ff
Pierlou <pierlou.ramade@data.gouv.fr>
2025-07-25 08:50:52 +0200

    refactor: rationalize common keywords for coords

--- csv_detective/detect_labels/geo/lonlat_wgs/__init__.py
+++ csv_detective/detect_labels/geo/lonlat_wgs/__init__.py
@@ -1,4 +1,5 @@
 from csv_detective.parsing.text import header_score
+from ..latlon_wgs import COMMON_COORDS_LABELS
 
 PROPORTION = 0.5
 
@@ -7,37 +8,7 @@ def _is(header: str) -> float:
     words_combinations_list = [
         "lonlat wgs",
         "lonlat",
-        "geo point",
-        "geo point 2d",
-        "wgs84",
-        "geolocalisation",
-        "geo",
-        "coordonnees finales",
-        "coordonnees",
-        "coordonnees ban",
-        "xy",
-        "geometry x y",
-        "coordonnees insee",
-        "coordonnees geographiques",
-        "position",
-        "coordonnes gps",
-        "geopoint",
-        "geom x y",
-        "coord gps",
         "longlat",
-        "position geographique",
-        "c geo",
-        "coordonnes geoloc",
         "lon lat",
-        "code geo",
-        "geo localisation",
-        "coordonnes geo",
-        "geo cp",
-        "x y",
-        "geo coordinates",
-        "point geo",
-        "point geo insee",
-        "coordonnees geoloc",
-        "coordonnees xy",
-    ]
+    ] + COMMON_COORDS_LABELS
     return header_score(header, words_combinations_list)
