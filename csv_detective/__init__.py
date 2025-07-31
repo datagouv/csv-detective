8ee4eb2721d0f285015c323f7430292f78520470
Pierlou <pierlou.ramade@data.gouv.fr>
2025-07-31 11:00:22 +0200

    chore: version is in pyproject

--- csv_detective/__init__.py
+++ csv_detective/__init__.py
@@ -1,4 +1,2 @@
 from csv_detective.explore_csv import routine, routine_minio, validate_then_detect  # noqa
 from csv_detective.output.example import create_example_csv_file  # noqa
-
-__version__ = '0.8.1.dev'
