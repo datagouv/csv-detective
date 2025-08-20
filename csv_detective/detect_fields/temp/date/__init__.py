1ed5a2769bceeaad8abf9cfeb36af46f1f657405
Pierlou <pierlou.ramade@data.gouv.fr>
2025-08-20 14:19:13 +0200

    refactor: restore regex checks in date for early stops

--- csv_detective/detect_fields/temp/date/__init__.py
+++ csv_detective/detect_fields/temp/date/__init__.py
@@ -1,9 +1,12 @@
+import re
 from datetime import datetime
 from typing import Optional
 
 from dateparser import parse as date_parser
 from dateutil.parser import ParserError
 from dateutil.parser import parse as dateutil_parser
+
+from csv_detective.parsing.text import _process_text
 
 PROPORTION = 1
 # /!\ this is only for dates, not datetimes which are handled by other utils
@@ -19,6 +22,19 @@ def date_casting(val: str) -> Optional[datetime]:
         return None
 
 
+seps = r"[\s/\-\*_\|;.,]"
+# matches JJ-MM-AAAA with any of the listed separators
+pat = r"^(0[1-9]|[12][0-9]|3[01])SEP(0[1-9]|1[0-2])SEP((19|20)\d{2})$".replace("SEP", seps)
+# matches AAAA-MM-JJ with any of the listed separators OR NO SEPARATOR
+tap = r"^((19|20)\d{2})SEP(0[1-9]|1[0-2])SEP(0[1-9]|[12][0-9]|3[01])$".replace("SEP", seps + "?")
+# matches JJ-mmm-AAAA and JJ-mmm...mm-AAAA with any of the listed separators OR NO SEPARATOR
+letters = (
+    r"^(0[1-9]|[12][0-9]|3[01])SEP(jan|fev|feb|mar|avr|apr"
+    r"|mai|may|jun|jui|jul|aou|aug|sep|oct|nov|dec|janvier|fevrier|mars|avril|"
+    r"mai|juin|jullet|aout|septembre|octobre|novembre|decembre)SEP"
+    r"([0-9]{2}$|(19|20)[0-9]{2}$)"
+).replace("SEP", seps + "?")
+
 threshold = 0.3
 
 
@@ -27,6 +43,16 @@ def _is(val):
     # early stops, to cut processing time
     if not isinstance(val, str) or len(val) > 20 or len(val) < 8:
         return False
+    # if it's a usual date pattern
+    if any(
+        # with this syntax, if the first value is True, the other two are not computed (and so on)
+        [
+            bool(re.match(pat, val))
+            or bool(re.match(tap, val))
+            or bool(re.match(letters, _process_text(val)))
+        ]
+    ):
+        return True
     if sum([char.isdigit() for char in val]) / len(val) < threshold:
         return False
     res = date_casting(val)
