266fdc9c9ef06ed617df217fe18d62233814e447
Pierlou <pierlou.ramade@data.gouv.fr>
2025-11-14 11:22:44 +0100

    refactor: better labels detection for latlon and lonlat

--- csv_detective/detect_labels/geo/latlon_wgs/__init__.py
+++ csv_detective/detect_labels/geo/latlon_wgs/__init__.py
@@ -3,44 +3,38 @@ from csv_detective.parsing.text import header_score
 PROPORTION = 0.5
 
 COMMON_COORDS_LABELS = [
-    "c geo",
-    "code geo",
-    "coord gps",
+    "ban",
+    "coordinates",
     "coordonnees",
-    "coordonnees ban",
-    "coordonnees finales",
-    "coordonnees geo",
-    "coordonnees geographiques",
-    "coordonnees geoloc",
-    "coordonnees geoloc",
-    "coordonnees gps",
     "coordonnees insee",
-    "coordonnees xy",
     "geo",
-    "geo coordinates",
-    "geo cp",
-    "geo localisation",
-    "geo point",
-    "geo point 2d",
-    "geolocalisation",
-    "geom x y",
-    "geometry x y",
     "geopoint",
-    "point geo",
-    "point geo insee",
+    "geoloc",
+    "geolocalisation",
+    "geom",
+    "geometry",
+    "gps",
+    "localisation",
+    "point",
     "position",
-    "position geographique",
     "wgs84",
+]
+
+specific = [
+    "latlon",
+    "lat lon",
     "x y",
     "xy",
 ]
 
+# we aim wide to catch exact matches if possible for the highest possible score
+words = COMMON_COORDS_LABELS + specific + [
+    w + sep + suf
+    for suf in specific
+    for w in COMMON_COORDS_LABELS
+    for sep in ["", " "]
+]
+
 
 def _is(header: str) -> float:
-    words_combinations_list = [
-        "latlon wgs",
-        "latlon",
-        "latlong",
-        "lat lon",
-    ] + COMMON_COORDS_LABELS
-    return header_score(header, words_combinations_list)
+    return header_score(header, words)
--- csv_detective/detect_labels/geo/lonlat_wgs/__init__.py
+++ csv_detective/detect_labels/geo/lonlat_wgs/__init__.py
@@ -4,12 +4,21 @@ from ..latlon_wgs import COMMON_COORDS_LABELS
 
 PROPORTION = 0.5
 
+specific = [
+    "lonlat",
+    "lon lat",
+    "y x",
+    "yx",
+]
+
+# we aim wide to catch exact matches if possible for the highest possible score
+words = COMMON_COORDS_LABELS + specific + [
+    w + sep + suf
+    for suf in specific
+    for w in COMMON_COORDS_LABELS
+    for sep in ["", " "]
+]
+
 
 def _is(header: str) -> float:
-    words_combinations_list = [
-        "lonlat wgs",
-        "lonlat",
-        "longlat",
-        "lon lat",
-    ] + COMMON_COORDS_LABELS
-    return header_score(header, words_combinations_list)
+    return header_score(header, words)
