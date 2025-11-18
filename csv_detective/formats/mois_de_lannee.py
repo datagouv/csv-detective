e88d8045568d5ee10f67e771d7733f7f334004c5
Pierlou <pierlou.ramade@data.gouv.fr>
2025-11-18 16:48:51 +0100

    draft:raft: POC of new structure

---
+++ csv_detective/formats/mois_de_lannee.py
@@ -0,0 +1,40 @@
+from unidecode import unidecode
+
+proportion = 1
+labels = ["fr", "temp"]
+mois = {
+    "janvier",
+    "fevrier",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "aout",
+    "septembre",
+    "octobre",
+    "novembre",
+    "decembre",
+    "jan",
+    "fev",
+    "mar",
+    "avr",
+    "mai",
+    "jun",
+    "jui",
+    "juil",
+    "aou",
+    "sep",
+    "sept",
+    "oct",
+    "nov",
+    "dec",
+}
+
+
+def _is(val):
+    """Renvoie True si les champs peuvent être des mois de l'année"""
+    if not isinstance(val, str):
+        return False
+    val = unidecode(val.lower())
+    return val in mois
