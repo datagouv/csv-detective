08f4b1810d2f461eedd60c9b84811ace71a67e64
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2025-12-03 14:58:04 +0100

    Better duplicates count (#182)

--- csv_detective/parsing/csv.py
+++ csv_detective/parsing/csv.py
@@ -42,7 +42,7 @@ def parse_csv(
                 logging.warning(f"File is too long, analysing in chunks of {CHUNK_SIZE} rows")
             total_lines, nb_duplicates = None, None
         else:
-            nb_duplicates = len(table.loc[table.duplicated()])
+            nb_duplicates = table.duplicated().sum()
         if num_rows > 0:
             num_rows = min(num_rows, total_lines or len(table))
             table = table.sample(num_rows, random_state=random_state)
