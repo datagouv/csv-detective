8171d9983daecee8a4a44e67c00eca63b08debfc
Pierlou <pierlou.ramade@data.gouv.fr>
2025-12-04 12:56:52 +0100

    refactor: float accepts sc. notation

--- csv_detective/formats/float.py
+++ csv_detective/formats/float.py
@@ -1,6 +1,10 @@
+import re
+
 proportion = 1
 tags = ["type"]
 labels = ["part", "ratio", "taux"]
+
+scientific_notation_pattern = r"\d+\.\d+[e|E][+|-]?\d+"
 
 
 def float_casting(val: str) -> float:
@@ -8,13 +12,17 @@ def float_casting(val: str) -> float:
 
 
 def _is(val):
-    """Detects floats, assuming that tables will not have scientific
-    notations (3e6) or "+" in the string. "-" is still accepted."""
+    """Detects floats (including scientific notation), unless there is an underscore or a plus sign (bad practice)."""
     try:
         if (
             not isinstance(val, str)
-            or any([k in val for k in ["_", "+", "e", "E"]])
+            or "_" in val
             or (val.startswith("0") and len(val) > 1 and val[1] not in [".", ","])
+        ):
+            return False
+        elif (
+            any([k in val for k in ["+", "e", "E"]])
+            and not re.match(scientific_notation_pattern, val)
         ):
             return False
         float_casting(val)
@@ -24,6 +32,6 @@ def _is(val):
 
 
 _test_values = {
-    True: ["1", "0", "1764", "-24", "1.2", "1863.23", "-12.7", "0.1"],
+    True: ["1", "0", "1764", "-24", "1.2", "1863.23", "-12.7", "0.1", "1.9764E-1", "19.01e-29"],
     False: ["01053", "01053.89", "1e3", "123_456", "123_456.78", "+35", "+35.9"],
 }
