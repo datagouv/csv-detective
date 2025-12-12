4ad182dec3327fd5023cc8ec1dd53e1faaf3a84d
Pierlou <pierlou.ramade@data.gouv.fr>
2025-12-12 17:00:40 +0100

    refactor: better codes iso alpha

--- csv_detective/formats/iso_country_code_alpha3.py
+++ csv_detective/formats/iso_country_code_alpha3.py
@@ -7,17 +7,17 @@ proportion = 1
 tags = ["geo"]
 
 with open(join(dirname(__file__), "data", "iso_country_code_alpha3.txt"), "r") as iofile:
-    liste_pays = iofile.read().split("\n")
+    liste_pays = set(iofile.read().split("\n"))
 
 
 def _is(val):
     """Renvoie True si val peut etre un code iso pays alpha-3, False sinon"""
-    if not isinstance(val, str) or not bool(re.match(r"[A-Z]{3}$", val)):
+    if not isinstance(val, str) or not bool(re.match(r"[a-zA-Z]{3}$", val)):
         return False
-    return val in set(liste_pays)
+    return val.upper() in liste_pays
 
 
 _test_values = {
-    True: ["FRA"],
+    True: ["FRA", "brb"],
     False: ["XXX", "FR", "A"],
 }
